3e9b9fde4d1bb5eb91f9e464bf8a870e6087f478
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-11-16 14:59:26 -0600

    char based compression implemented

--- internal/radix/radix.go
+++ internal/radix/radix.go
@@ -1,6 +1,10 @@
 package radix
 
-import "github.com/elmq0022/krillin/router"
+import (
+	"fmt"
+
+	"github.com/elmq0022/krillin/router"
+)
 
 type Node[T any] struct {
 	prefix   string
@@ -12,10 +16,63 @@ type Radix[T any] struct {
 	root *Node[T]
 }
 
-func New[T any]([]router.Route[T]) (*Radix[T], error) {
-	return &Radix[T]{}, nil
+func New[T any](routes []router.Route[T]) (*Radix[T], error) {
+	r := Radix[T]{root: &Node[T]{}}
+
+	for _, route := range routes {
+		if len(route.Path) == 0 || route.Path[0] != '/' {
+			return nil, fmt.Errorf("path must start with '/'")
+		}
+		rs := []rune(route.Path)
+		r.addRoute(route, r.root, rs, 0)
+	}
+
+	compress(r.root)
+
+	return &r, nil
+}
+
+func (r *Radix[T]) addRoute(route router.Route[T], root *Node[T], rs []rune, pos int) {
+	if pos >= len(rs) {
+		if root.terminal == nil {
+			root.terminal = make(map[string]T)
+		}
+		root.terminal[route.Method] = route.Handler
+		return
+	}
+
+	c := rs[pos]
+	for _, node := range root.children {
+		if node.prefix == string(c) {
+			r.addRoute(route, node, rs, pos+1)
+			return
+		}
+	}
+
+	n := &Node[T]{prefix: string(c)}
+	root.children = append(root.children, n)
+	r.addRoute(route, n, rs, pos+1)
+}
+
+func compress[T any](n *Node[T]) {
+
+	for i := range n.children {
+		compress(n.children[i])
+	}
+
+	if len(n.children) == 1 && n.terminal == nil {
+		child := n.children[0]
+
+		n.prefix += child.prefix
+		n.terminal = child.terminal
+		n.children = child.children
+	}
 }
 
 func (r *Radix[T]) Lookup(method, path string) (T, bool) {
 	return *new(T), true
+}
+
+func (r *Radix[T]) FirstPrefix() string {
+	return r.root.prefix
 }
--- internal/radix/radix_test.go
+++ internal/radix/radix_test.go
@@ -4,23 +4,23 @@ import (
 	"net/http"
 	"testing"
 
-	"github.com/elmq0022/krillin/adapters"
 	"github.com/elmq0022/krillin/internal/radix"
 	"github.com/elmq0022/krillin/router"
 )
 
-func TestRadix(t *testing.T) {
-	routes := []router.Route[adapters.Handler]{}
+func TestNewRadix(t *testing.T) {
 
-	radix, _ := radix.New[adapters.Handler](routes)
-
-	url := "/url/path/to/resource"
+	path := "/url/path/to/resource"
 	method := http.MethodGet
-	req, _ := http.NewRequest(method, url, nil)
+	handler := 1
 
-	_, ok := radix.Lookup(req.Method, req.URL.Path)
-	if !ok {
-		t.Fatal("Lookup was not OK")
+	routes := []router.Route[int]{
+		{Path: path, Method: method, Handler: handler},
 	}
-	// handler(req)
+
+	r, _ := radix.New(routes)
+
+	if r.FirstPrefix() != path {
+		t.Fatalf("want: %s, got %s", path, r.FirstPrefix())
+	}
 }
