fad1804576ad8951f536b01f8d0552e6f6677f5b
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-11-20 20:42:41 -0600

    table driven test

--- router/router_test.go
+++ router/router_test.go
@@ -1,6 +1,7 @@
 package router_test
 
 import (
+	"maps"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,17 +17,13 @@ type SpyAdapterRecord struct {
 	Params map[string]string
 }
 
-func NewSpyAdapter(records *[]SpyAdapterRecord) types.Adapter {
+func NewSpyAdapter(record *SpyAdapterRecord) types.Adapter {
 	return func(w http.ResponseWriter, r *http.Request, h types.Handler) {
 		status, body, err := h(r)
-
-		record := SpyAdapterRecord{}
 		record.Status = status
 		record.Body = body
 		record.Err = err
 		record.Params = router.GetParams(r.Context())
-
-		*records = append(*records, record)
 	}
 }
 
@@ -37,26 +34,46 @@ func NewTestHandler(status int, body any, err error) types.Handler {
 }
 
 func TestRouter_BasicRoutes(t *testing.T) {
-	want := 1
-
-	routes := types.Routes{
-		{Method: http.MethodGet, Path: "/", Handler: NewTestHandler(http.StatusOK, want, nil)},
-		// {Method: http.MethodGet, Path: "/:id", Handler: paramsHandler},
+	tests := []struct {
+		name       string
+		method     string
+		path       string
+		wantStatus int
+		wantBody   any
+		wantErr    error
+		wantParams map[string]string
+		callPath   string
+	}{
+		{name: "root", method: http.MethodGet, path: "/", wantStatus: http.StatusOK, wantBody: "root", wantErr: nil, wantParams: map[string]string{}, callPath: "/"},
+		{name: "root", method: http.MethodGet, path: "/book/:id", wantStatus: http.StatusOK, wantBody: "books", wantErr: nil, wantParams: map[string]string{"id": "lifeOfPi"}, callPath: "/book/lifeOfPi"},
 	}
 
-	records := []SpyAdapterRecord{}
-	r := router.New(routes, NewSpyAdapter(&records))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			record := SpyAdapterRecord{}
+			routes := types.Routes{{Method: tt.method, Path: tt.path, Handler: NewTestHandler(tt.wantStatus, tt.wantBody, tt.wantErr)}}
+			r := router.New(routes, NewSpyAdapter(&record))
 
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	rec := httptest.NewRecorder()
+			req := httptest.NewRequest(tt.method, tt.callPath, nil)
+			rec := httptest.NewRecorder()
 
-	r.ServeHTTP(rec, req)
+			r.ServeHTTP(rec, req)
 
-	if want != records[0].Body {
-		t.Fatalf("want %v, got %v", want, records[0].Body)
-	}
+			if tt.wantBody != record.Body {
+				t.Fatalf("body: want %v, got %v", tt.wantBody, record.Body)
+			}
 
-	if records[0].Err != nil {
-		t.Fatalf("want nil, got %q", records[0].Err)
+			if tt.wantStatus != record.Status {
+				t.Fatalf("status: want %d, got %d", tt.wantStatus, record.Status)
+			}
+
+			if tt.wantErr != record.Err {
+				t.Fatalf("error: want %v got %v", tt.wantErr, record.Err)
+			}
+
+			if !maps.Equal(tt.wantParams, record.Params) {
+				t.Fatalf("params: want %v got %v", tt.wantParams, record.Params)
+			}
+		})
 	}
 }
