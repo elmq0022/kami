45ab2ba2c9e5f7d22a42f2223bd1b5c0f54b3a44
Aaron Elmquist <aaron.elmquist@protonmail.com>
2025-11-22 03:00:32 -0600

    adds custom 404 opt

--- router/options.go
+++ router/options.go
@@ -1,13 +1,17 @@
 package router
 
+import "github.com/elmq0022/kami/types"
+
 type Option func(r *Router)
 
 func WithMiddleware() Option {
 	return func(r *Router) {}
 }
 
-func WithNotFound() Option {
-	return func(r *Router) {}
+func WithNotFound(h types.Handler) Option {
+	return func(r *Router) {
+		r.notFound = h
+	}
 }
 
 func WithLogger() Option {
--- router/options_test.go
+++ router/options_test.go
@@ -1,1 +1,38 @@
 package router_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/elmq0022/kami/router"
+	"github.com/elmq0022/kami/types"
+)
+
+func TestWithMiddleware(t *testing.T) {
+	// r, _ := router.New(NewSpyAdapter(&SpyAdapterRecord{}), router.WithMiddleware())
+
+}
+
+func TestWithNotFound(t *testing.T) {
+	testNotFound := func(r *http.Request) (types.Response, error) {
+		return types.Response{
+				Status: http.StatusNotFound,
+				Body:   "test not found"},
+			nil
+	}
+	spy := SpyAdapterRecord{}
+	r, _ := router.New(NewSpyAdapter(&spy), router.WithNotFound(testNotFound))
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	r.ServeHTTP(rec, req)
+
+	if spy.Status != http.StatusNotFound {
+		t.Fatalf("want %d got %d", http.StatusNotFound, spy.Status)
+	}
+
+	if spy.Body != "test not found" {
+		t.Fatalf("want %s, got %s", "test not found", spy.Body)
+	}
+}
